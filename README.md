e98d1bde0f3e5a2ab73174fa43467425d4bee18c
Henry Lee <henryleeworld@gmail.com>
2023-04-04 00:04:55 +0800

    feat:初始化

---
+++ README.md
@@ -0,0 +1,35 @@
+# Laravel 8 Livewire 紀錄使用者認證細節並寄送新裝置通知
+
+引入 rappasoft 的 laravel-authentication-log 套件來擴增紀錄使用者認證細節並寄送新裝置通知，用於追蹤使用者認證訊息，例如登入/登出時間、IP、瀏覽器、位置等，並通過郵件、slack 或簡訊寄送新設備和登入失敗的通知。
+
+## 使用方式
+- 把整個專案複製一份到你的電腦裡，這裡指的「內容」不是只有檔案，而是指所有整個專案的歷史紀錄、分支、標籤等內容都會複製一份下來。
+```sh
+$ git clone
+```
+- 將 __.env.example__ 檔案重新命名成 __.env__，如果應用程式金鑰沒有被設定的話，你的使用者 sessions 和其他加密的資料都是不安全的！
+- 當你的專案中已經有 composer.lock，可以直接執行指令以讓 Composer 安裝 composer.lock 中指定的套件及版本。
+```sh
+$ composer install
+```
+- 產生 Laravel 要使用的一組 32 字元長度的隨機字串 APP_KEY 並存在 .env 內。
+```sh
+$ php artisan key:generate
+```
+- 執行 __Artisan__ 指令的 __migrate__ 來執行所有未完成的遷移。
+```sh
+$ php artisan migrate
+```
+- 執行安裝 Laravel Mix 引用的依賴項目，並執行所有 Mix 任務。
+```sh
+$ npm install && npm run dev
+```
+- 在瀏覽器中輸入已定義的路由 URL 來訪問，例如：http://127.0.0.1:8000。
+- 你可以經由 `/register` 來進行註冊。
+- 完成註冊後，可以經由 `/login` 來進行登入。
+
+----
+
+## 畫面截圖
+![](https://i.imgur.com/1Zudc1F.png)
+> 紀錄使用者認證訊息
---
+++ app/Http/Livewire/AuthenticationLog.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use JamesMills\LaravelTimezone\Facades\Timezone;
+use Jenssegers\Agent\Agent;
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelAuthenticationLog\Models\AuthenticationLog as Log;
+
+class AuthenticationLog extends DataTableComponent
+{
+    public string $defaultSortColumn = 'login_at';
+    public string $defaultSortDirection = 'desc';
+    public string $tableName = 'authentication-log';
+
+    public User $user;
+
+    public function mount(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make(__('IP Address'), 'ip_address')
+                ->searchable(),
+            Column::make(__('Browser'), 'user_agent')
+                ->searchable()
+                ->format(function($value) {
+                    $agent = tap(new Agent, fn($agent) => $agent->setUserAgent($value));
+                    return $agent->platform() . ' - ' . $agent->browser();
+                }),
+            Column::make(__('Location'))
+                ->searchable(function (Builder $query, $searchTerm) {
+                    $query->orWhere('location->city', 'like', '%'.$searchTerm.'%')
+                        ->orWhere('location->state', 'like', '%'.$searchTerm.'%')
+                        ->orWhere('location->state_name', 'like', '%'.$searchTerm.'%')
+                        ->orWhere('location->postal_code', 'like', '%'.$searchTerm.'%');
+                })
+                ->format(fn ($value) => $value && $value['default'] === false ? $value['city'] . ', ' . $value['state'] : '-'),
+            Column::make(__('Login At'), 'login_at')
+                ->sortable()
+                ->format(fn($value) => $value ? Timezone::convertToLocal($value) : '-'),
+            Column::make(__('Login Successful'), 'login_successful')
+                ->sortable()
+                ->format(fn($value) => $value === true ? __('Yes') : __('No')),
+            Column::make(__('Logout At'), 'logout_at')
+                ->sortable()
+                ->format(fn($value) => $value ? Timezone::convertToLocal($value) : '-'),
+            Column::make(__('Cleared By User'), 'cleared_by_user')
+                ->sortable()
+                ->format(fn($value) => $value === true ? __('Yes') : __('No')),
+        ];
+    }
+
+    public function query(): Builder
+    {
+        return Log::query()
+            ->where('authenticatable_type', User::class)
+            ->where('authenticatable_id', $this->user->id);
+    }
+}
